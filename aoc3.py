dd1dd7f10a78f7ecbb7557e15463023c49467a79
Robert Nolet <45286978+RobertNolet@users.noreply.github.com>
2025-12-03 19:03:36 +0100

    solution aoc3

--- aoc3.py
+++ aoc3.py
@@ -1,1 +1,15 @@
-data = [map(int, line) for line in open('input.txt').readlines()]
+data = [[int(x) for x in line.strip()] for line in open('input.txt')]
+
+scores = {1:0, 2:0}
+for line in data:
+    for part, l in [(1, 2), (2, 12)]:
+        value = 0
+        i = -1
+        for digit in range(l):
+            n = max(line[i+1:len(line)-(l-1-digit)])
+            i += 1 + line[i+1:len(line)-(l-1-digit)].index(n)
+            value = 10*value + n
+        scores[part] += value
+
+print('Part 1:', scores[1])
+print('Part 2:', scores[2])
